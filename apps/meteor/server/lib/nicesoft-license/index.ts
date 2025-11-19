0a3fe04fcd4d3f74a95bcb68bee5d56ddf01ddfc
nicesoft-labs <me@sbelikov.ru>
2025-11-19 18:02:10 +0300

    Update index.ts

--- apps/meteor/server/lib/nicesoft-license/index.ts
+++ apps/meteor/server/lib/nicesoft-license/index.ts
@@ -1,9 +1,11 @@
 export { reloadLicense, getCurrentLicense } from './cache';
 export { isLicensed, hasFeature, getLimit } from './helpers';
 export {
-	getActiveUsersLimitInfo,
-	getGuestUsersLimitInfo,
-	isActiveUsersLimitReached,
-	isGuestUsersLimitReached,
+        getActiveUsersLimitInfo,
+        getGuestUsersLimitInfo,
+        isActiveUsersLimitReached,
+        isGuestUsersLimitReached,
 } from './limits';
+export { onLicenseChanged, onLimitReached, onLimitRestored } from './events';
+export { registerLimitCounter, shouldPreventAction, getLimitState } from './limitCounters';
 export type { NicesoftLicenseDocument, LicenseState } from './types';
