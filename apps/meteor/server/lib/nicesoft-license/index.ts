8441ac9ec45985a88fdc543ced5c34df80dc3f6e
nicesoft-labs <me@sbelikov.ru>
2025-11-19 16:01:59 +0300

    Create index.ts

---
+++ apps/meteor/server/lib/nicesoft-license/index.ts
@@ -0,0 +1,3 @@
+export { reloadLicense, getCurrentLicense } from './cache';
+export { isLicensed, hasFeature, getLimit } from './helpers';
+export type { NicesoftLicenseDocument, LicenseState } from './types';
